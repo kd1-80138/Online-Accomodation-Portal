1046b06187bafa1d0f06b55c9f85e6ddaee6af35
yashrajg09 <yashraj.ghadge28@gmail.com>
2024-02-11 11:54:33 +0530

    for_new_changes

--- Backend/online_accomodation_portal/online_accomodation_portal/src/main/java/com/portal/custom_exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.portal.custom_exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String mesg) {
-		super(mesg);
-	}
-}
--- Backend/online_accomodation_portal/online_accomodation_portal/src/main/java/com/portal/dao/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portal.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.portal.entities.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
